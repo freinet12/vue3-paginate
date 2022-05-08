76b6533c78ca00a00a278457e0d2fa37d646efc5
Freinet Brutus <freinetbrutus@gmail.com>
2022-05-08 15:10:52 -0400

    Updating for including 'mitt' dependency

--- dev/serve.ts
+++ dev/serve.ts
@@ -1,13 +1,11 @@
 import { createApp } from 'vue';
 import Dev from './serve.vue';
-import mitt from 'mitt';
-const emitter = mitt();
+
 // To register individual components where they are used (serve.vue) instead of using the
 // library as a whole, comment/remove this import and it's corresponding "app.use" call
 import Vue3Paginate from '@/entry.esm';
 
 const app = createApp(Dev);
-app.config.globalProperties.emitter = emitter;
 app.use(Vue3Paginate);
 
 app.mount('#app');
--- src/entry.esm.ts
+++ src/entry.esm.ts
@@ -1,10 +1,13 @@
 import { App, Plugin } from 'vue';
+import mitt from 'mitt';
+const emitter = mitt();
 
 // Import vue components
 import * as components from '@/lib-components/index';
 
 // install function executed by Vue.use()
 const install: Exclude<Plugin['install'], undefined> = function installVue3Paginate(app: App) {
+  app.config.globalProperties.emitter = emitter;
   Object.entries(components).forEach(([componentName, component]) => {
     app.component(componentName, component);
   });
